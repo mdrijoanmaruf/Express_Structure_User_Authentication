d776f9fadcfa21e11c10becaf86bc800c5647859
Md Rijoan Maruf <rmsaif4430@gmail.com>
2026-06-28 01:17:49 +0600

    user interface

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,13 @@
 import { pool } from "../../db/index.js";
+import type { IUser } from "./user.interface.js";
 
-const createUserIntoDB = async(payload : any) => {
-    const {name, email, password, is_active, age, created_at, updated_at} = payload;
+const createUserIntoDB = async(payload : IUser) => {
+    const {name, email, password, is_active, age} = payload;
     const result = await pool.query(
       `
         INSERT INTO users(name , email , password , is_active , age , created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *
         `,
-      [name, email, password, is_active, age, created_at, updated_at],
+      [name, email, password, is_active, age],
     );
     return result;
 }
